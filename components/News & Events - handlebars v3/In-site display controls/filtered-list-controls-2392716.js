8e85a38870c1ed13729402e1883dd393f9987af4
tomstringer <t.stringer@unimelb.edu.au>
2017-06-29 09:00:20 +1000

    Add new script loader file

---
+++ components/News & Events - handlebars v3/In-site display controls/filtered-list-controls-2392716.js
@@ -0,0 +1,23 @@
+var cssId = 'filteredListingCSS';
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = './?a=1673063';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+var jQueryId = 'filteredListingJS';
+if (!document.getElementById(jQueryId))
+{
+    var body  = document.getElementsByTagName('body')[0];
+    var link  = document.createElement('script');
+    link.id   = jQueryId;
+    link.type = 'text/javascript';
+    link.src = './?a=2392839';
+    body.appendChild(link);
+}
